ac13bde64648bde69b5d6e3d13c0d14d060680ee
hasibul hasan <hasibul.hasan@sslwireless.com>
2021-06-16 10:46:03 +0600

    established graphql with express

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const {connectDB} = require('./db');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./graphql/schema');
 
 dotenv.config();
 connectDB();
 
 app.get('/', (req, res) => {
-    res.json({msg: 'Hello world'})
+    res.json({msg: 'Welcome to Graphql Blog'})
 });
+
+app.use("/graphql", graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
 
 app.listen(process.env.PORT, () => {
     console.log(`App running on PORT ${process.env.PORT}`);
